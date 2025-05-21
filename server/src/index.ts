08f81a1b294de1cccc7d345bc78b5a5fd263f14b
Steve <stevedsimkins@gmail.com>
2025-05-21 16:26:31 -0400

    chore: Added wrangler setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,6 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import type { ApiResponse, Pledge, ServerBindings } from "shared/dist";
-import { createPledge } from "./db";
 import { createClient } from "@supabase/supabase-js";
 
 const app = new Hono<{ Bindings: ServerBindings }>();
@@ -38,55 +37,19 @@ app.get("/pledges", async (c) => {
       }
 
       allPledges = [...allPledges, ...users]
-      if(users.length < 1000) {
+      if (users.length < 1000) {
         hasMore = false;
       } else {
         page = page + 1;
       }
     }
 
-    return c.json({ data: allPledges.length });
+    return c.json({ data: allPledges });
   } catch (error) {
     console.log(error);
     return c.json({ message: "Server Error" }, 500);
   }
 });
 
-app.post("/pledges", async (c) => {
-  const token = c.req.header("X-Orbiter-Token");
-
-  if (!token) {
-    return c.json({ error: "Unauthenticated" }, { status: 401 });
-  }
-
-  try {
-    const supabase = createClient(
-      c.env.SUPABASE_URL,
-      c.env.SUPABASE_SERVICE_ROLE_KEY
-    );
-
-    const {
-      data: { user },
-    } = await supabase.auth.getUser(token);
-
-    const pledge: Pledge = {
-      user_id: "",
-      email: "",
-      display_name: "",
-      avatar_url: "",
-    };
-
-    await createPledge(c, pledge);
-
-    const data: ApiResponse = {
-      message: "Pledge Stored!",
-      success: true,
-    };
-    return c.json(data, { status: 200 });
-  } catch (error) {
-    console.log(error);
-    return c.json({ error: "Problem creating pledge" }, { status: 500 });
-  }
-});
 
 export default app;
